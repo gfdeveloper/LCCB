e3b02c5a829016d2a6fcac617ee416fbe889d683
Chris L. Carver <christopher.carver@globalfoundries.com>
2016-01-20 11:46:53 -0500

    Test

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -7,7 +7,7 @@
                 <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span>
                 <span class="icon-bar"></span> <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="\">LCCB</a>
+            <a class="navbar-brand" href="\">LCCB!</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
